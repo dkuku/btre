9ee3d661051be8682b0cab11c1ac65c129b49995
Daniel Kukula <daniel.kuku@gmail.com>
2019-08-31 20:54:53 +0100

    media and statuc folders

--- btre/settings.py
+++ btre/settings.py
@@ -125,3 +125,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "btre/static")]
+
+# Media folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
